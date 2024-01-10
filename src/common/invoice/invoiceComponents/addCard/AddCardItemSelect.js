495001c591f40a257ac2a701ac47e6e2335eb4fd
sa-lik <balisaalik@gmail.com>
2024-01-10 12:29:45 +0500

    updated app name from package-lock

--- src/common/invoice/invoiceComponents/addCard/AddCardItemSelect.js
+++ src/common/invoice/invoiceComponents/addCard/AddCardItemSelect.js
@@ -7,7 +7,7 @@ import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 
-const AddCardItemSelect = ({visaBookingIds}) => {
+const AddCardItemSelect = ({ visaBookingIds }) => {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label='invoice table'>

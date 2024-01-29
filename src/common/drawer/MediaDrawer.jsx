3c01c59e479af63958927358f1b8135c26a95fc9
crownintldev <crownintltechnology@gmail.com>
2024-01-29 14:10:52 +0500

    media drawer ui, api data fetch

--- src/common/drawer/MediaDrawer.jsx
+++ src/common/drawer/MediaDrawer.jsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import TreeView from '@mui/lab/TreeView'
 import TreeItem from '@mui/lab/TreeItem'
+import Icon from 'src/@core/components/icon'
 
 const MediaDrawer = ({ open, onClose, data }) => {
   const [expanded, setExpanded] = useState([])
@@ -32,28 +33,69 @@ const MediaDrawer = ({ open, onClose, data }) => {
           onNodeToggle={handleTreeItemToggle}
         >
           <TreeItem nodeId='passportId' label='passportId'>
-            {data.passportId.files.map((item, index) => (
-              <>
-                <TreeItem
-                  key={`${index}+${item.fileName}`}
-                  nodeId={`${index}+${item.fileName}`}
-                  label={`fileType: ${item.fileName}`}
-                />
-                <TreeItem
+            {data.passportId.files ? (
+              data.passportId.files.map((item, index) => (
+                <>
+                  <TreeItem
+                    key={`${index}+${item.fileName}`}
+                    nodeId={`${index}+${item.fileName}`}
+                    label={
+                      <div>
+                        {item.fileType === 'image/png' ? (
+                          <Icon icon='tabler:file-type-png' width={24} height={24} />
+                        ) : item.fileType === 'image/jpeg' ? (
+                          <Icon icon='tabler:file-type-png' width={24} height={24} />
+                        ) : (
+                          <span>{item.fileName}</span>
+                        )}
+                        {item.fileName}
+                      </div>
+                    }
+                  />
+                  <TreeItem
+                    key={`${index}+${item.fileName}`}
+                    nodeId={`${index}+${item.fileName}`}
+                    label={
+                      <div>
+                        {item.fileType === 'image/png' ? (
+                          <Icon icon='tabler:file-type-png' width={24} height={24} />
+                        ) : item.fileType === 'image/jpeg' ? (
+                          <Icon icon='tabler:file-type-jpg' width={24} height={24} />
+                        ) : (
+                          <span>{item.fileName}</span>
+                        )}
+                        {item.fileName}
+                      </div>
+                    }
+                  />
+                  {/* <TreeItem
                   key={`${index}+${item.fileType}`}
                   nodeId={`${index}+${item.fileType}`}
-                  label={`fileType: ${item.fileType}`}
-                />
-                <TreeItem
-                  key={`${index}+${item.fileUrl}`}
-                  nodeId={`${index}+${item.fileUrl}`}
-                  label={`fileUrl: ${item.fileUrl.substring(0, 35)}`}
-                  onClick={event =>
-                    handleTreeItemClick(event, `${index}+${item.fileUrl}`, item.fileUrl)
+                  label={
+                    <div>
+                      <Icon icon='tabler:file-type-jpg' width={24} height={24} />
+                      {`filename: ${item.fileName}`}
+                    </div>
                   }
-                />
-              </>
-            ))}
+                /> */}
+                  <TreeItem
+                    key={`${index}+${item.fileUrl}`}
+                    nodeId={`${index}+${item.fileUrl}`}
+                    label={
+                      <div>
+                        <Icon icon='tabler:link' width={24} height={24} />
+                        {`filename: ${item.fileName}`}
+                      </div>
+                    }
+                    onClick={event =>
+                      handleTreeItemClick(event, `${index}+${item.fileUrl}`, item.fileUrl)
+                    }
+                  />
+                </>
+              ))
+            ) : (
+              <h3>No Files Found...</h3>
+            )}
           </TreeItem>
         </TreeView>
         <Button variant='contained' color='primary' onClick={onClose} sx={{ mt: 10 }}>

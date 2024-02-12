9045640239c5503f46e7032a737251afbf116a84
crownintldev <crownintltechnology@gmail.com>
2024-02-12 15:36:58 +0500

    tailwind configured

--- src/common/forms/booking/passport/PassportForm.jsx
+++ src/common/forms/booking/passport/PassportForm.jsx
@@ -163,13 +163,10 @@ const PassportForm = ({ toggle, removeSelection, setFormSize, _id = '' }) => {
     }
   }, [passportNumber, _id])
 
-  // console.log(editId)
-
   useEffect(() => {
     setValue('deletedFiles', removeFiles)
   }, [previousFiles, setRemoveFiles])
-// console.log("removeFiles",removeFiles)
-// console.log("getValue",getValues("deletedFiles"))
+
   const watchedOnModel = watch('onModel')
 
   const handleClose = () => {
@@ -180,8 +177,7 @@ const PassportForm = ({ toggle, removeSelection, setFormSize, _id = '' }) => {
 
   const passportField1 = [
     {
-      name: 'passportNumber',
-      required: true
+      name: 'passportNumber'
     },
     {
       name: 'bookletNumber'
